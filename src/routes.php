99ce2a6ccbcf3a42446cce99317492491a7bc981
aslampangestu <aslampangestu@gmail.com>
2018-12-17 14:09:47 +0700

    add API kategori

--- src/routes.php
+++ src/routes.php
@@ -91,3 +91,78 @@ $app->delete("/api/v1/barang/{id}", function (Request $request, Response $respon
     
     return $response->withJson(["status" => "failed", "data" => "0"], 200);
 })->add($cekAPIKey);
+
+$app->get("/api/v1/kategori/", function (Request $request, Response $response){
+    $sql = "SELECT * FROM kategori";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->get("/api/v1/kategori/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $sql = "SELECT * FROM kategori WHERE id=:id";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->get("/api/v1/kategori", function (Request $request, Response $response, $args){
+    $keyword = $request->getQueryParam("name");
+    $sql = "SELECT * FROM kategori WHERE barang.name LIKE '%$keyword%'";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->post("/api/v1/kategori/", function (Request $request, Response $response){
+
+    $new_book = $request->getParsedBody();
+
+    $sql = "INSERT INTO kategori (name ) VALUES (:name)";
+    $stmt = $this->db->prepare($sql);
+
+    $data = [
+        ":name" => $new_book["name"]
+    ];
+
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
+})->add($cekAPIKey);
+
+$app->put("/api/v1/kategori/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $new_book = $request->getParsedBody();
+    $sql = "UPDATE kategori SET name=:name WHERE id=:id";
+    $stmt = $this->db->prepare($sql);
+    
+    $data = [
+        ":id" => $id,
+        ":name" => $new_book["name"]
+    ];
+
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
+})->add($cekAPIKey);
+
+$app->delete("/api/v1/kategori/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $sql = "DELETE FROM kategori WHERE id=:id";
+    $stmt = $this->db->prepare($sql);
+    
+    $data = [
+        ":id" => $id
+    ];
+
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
+})->add($cekAPIKey);
